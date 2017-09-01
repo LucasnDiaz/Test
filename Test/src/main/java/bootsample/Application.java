d4078714dff7edef8ebe9a2ca0048efdc8c5d21c
LucasnDiaz <lucasndiaz@gmail.com>
2017-09-01 11:54:57 -0300

    Tomcat configuration

--- Test/src/main/java/bootsample/Application.java
+++ Test/src/main/java/bootsample/Application.java
@@ -2,14 +2,21 @@ package bootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 	
 
 }
